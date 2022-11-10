a776cbe072c9e1bb8c9e32d1c0acabab997e3585
IagoFreitas12 <88669758+IagoFreitas12@users.noreply.github.com>
2022-11-10 15:32:05 -0300

    itens de entrega table

---
+++ database/migrations/2022_11_10_172533_create_itens_de_entrega_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('itens_de_entrega', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_id')->constrained();
+            $table->foreignId('entrega_id')->constrained();
+            $table->foreignId('status_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('itens_de_entrega');
+    }
+};
