c1bdcd7d7f1e7118034908a1b52a2844925883f0
IagoFreitas12 <88669758+IagoFreitas12@users.noreply.github.com>
2022-11-20 17:01:04 -0300

    endereco table relationship

---
+++ app/Models/Endereco.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Endereco extends Model
+{
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    use HasFactory;
+}
