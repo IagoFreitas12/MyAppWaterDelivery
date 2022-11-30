32317233becdaa300aa3ae667180ce577cc0b8b9
IagoFreitas12 <88669758+IagoFreitas12@users.noreply.github.com>
2022-11-30 19:36:32 -0300

    creating relationship to categoria e produto

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Categoria;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // cria 5 categorias com id de 1 a 5
+        for($id = 1 ; $id <= 5 ; $id++){
+            Categoria::create($this->makeCategoria($id));
+        }
+    }
+
+    public function makeCategoria($id) {
+        $faker = Faker::create();
+        return [
+            'rotulo' => $faker->domainWord(),
+            'id' => $id
+        ];
+    }
+}
---
+++ database/seeders/ProdutoSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Categoria;
+use App\Models\Estoque;
+use App\Models\Produto;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class ProdutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            CategoriaSeeder::class
+        ]);
+
+        $categorias = Categoria::all()->toArray();
+        $faker = Faker::create();
+        for($j = 0; $j < count($categorias); $j++) {
+            for($i = 1; $i <=10; $i++) {
+                Produto::create($this->makeProduto($i +$j*10, $categorias[$j]['id']));
+                Estoque::create([
+                    'id' => $i +$j*10,
+                    'produto_id' => $i +$j*10,
+                    'quantidade' => $faker->randomNumber(2, false)
+                ]);
+            }
+        }
+    }
+
+    public function makeProduto($id, $categoria_id) {
+        $faker = Faker::create();
+        return [
+            'nome' => $faker->name(),
+            'preco' => $faker->randomFloat(2, 1, 25),
+            'categoria_id' => $categoria_id,
+            'id' => $id
+        ];
+    }
+}
